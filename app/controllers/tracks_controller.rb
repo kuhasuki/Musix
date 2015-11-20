63d714bd7b8eba732931c3506681c37076c1aa0d
e-sacredgrounds <kuhasuki@gmail.com>
2015-11-19 18:06:44 -0800

    partially implement tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -10,17 +10,38 @@ class TracksController < ApplicationController
 	end
 
 	def destroy
+		@track = Track.find(params[:id])
+		if @track.destroy
+			redirect_to bands_url
+		else
+			redirect_to bands_url
+		end
 	end
 
 	def new
+		@albums = Album.all
+		@album = Album.new
+		@track = Track.new
+		@album.title = params[:album_name] if params[:album_name]
 	end
 
 	def create
+		@track = Track.new(track_params)
+		if @track.save
+			redirect_to bands_url
+		else
+			@albums = Album.all
+			@album = Album.new
+			@track = Track.new
+			@album.title = params[:album_name] if params[:album_name]
+			render :new
+		end
 	end
 
 	private
 
 	def track_params
+		params.require(:track).permit(:album_id, :name, :bonus_track, :lyrics)
 	end
 
 end
